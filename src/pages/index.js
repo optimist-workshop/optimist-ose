87dc1cfa402ad77398dffa05329d5ba2b06b6e42
canertol <m.canertol@gmail.com>
2025-04-08 16:30:33 -0400

    add tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+        <p className={styles.heroMotto}>From Learners to Doers</p> 
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
